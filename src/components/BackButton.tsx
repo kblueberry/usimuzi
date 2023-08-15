303cef02f78f3a8e6554c450ff0b38bc2cfe3208
Khrystyna Derenivska <khrisderenivska@gmail.com>
2023-08-15 12:23:40 +0300

    back button via text button not a link button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,13 +1,14 @@
-import { Link } from "@mui/material";
+import { Button } from "@mui/material";
 import { CONSTANTS } from "../constants/Constants";
 import { useNavigate } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export default function BackButton() {
   const navigate = useNavigate();
 
   return (
-    <Link className="link_action back_button" onClick={() => navigate("/")}>
-      {CONSTANTS.actions.backToUsers}
-    </Link>
+    <Button startIcon={<ArrowBackIcon />} onClick={() => navigate("/")}>
+      {CONSTANTS.actions.back}
+    </Button>
   );
 }
