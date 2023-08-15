d168052661652be1cd2ecd845f4ec258feddbe3d
Khrystyna Derenivska <khrisderenivska@gmail.com>
2023-08-15 14:34:12 +0300

    layout for search input & button

--- src/components/users/Search.tsx
+++ src/components/users/Search.tsx
@@ -1,15 +1,18 @@
 import { Button, TextField } from "@mui/material";
 import { CONSTANTS } from "../../constants/Constants";
+import "../users/Search.scss";
 
 export default function Search() {
   return (
-    <>
+    <div className="search_container">
       <TextField
         id="standard-basic"
         label={CONSTANTS.placeholders.search}
         variant="standard"
       />
-      <Button variant="contained">{CONSTANTS.actions.search}</Button>
-    </>
+      <Button className="search_button" variant="contained">
+        {CONSTANTS.actions.search}
+      </Button>
+    </div>
   );
 }
